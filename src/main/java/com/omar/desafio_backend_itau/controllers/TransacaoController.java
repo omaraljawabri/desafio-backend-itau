1d63fa8ea8694f5f34d28969b4ce1dcd344c6ede
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-02-03 19:51:23 -0300

    Endpoint de remoção das transações na classe TransacaoController

--- src/main/java/com/omar/desafio_backend_itau/controllers/TransacaoController.java
+++ src/main/java/com/omar/desafio_backend_itau/controllers/TransacaoController.java
@@ -5,10 +5,7 @@ import com.omar.desafio_backend_itau.services.TransacaoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +18,11 @@ public class TransacaoController {
     public ResponseEntity<Void> salvarTransacao(@RequestBody TransacaoRequestDTO transacaoRequestDTO){
         transacaoService.salvarTransacao(transacaoRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> removerTransacoes(){
+        transacaoService.removerTransacoes();
+        return ResponseEntity.ok().build();
     }
 }
