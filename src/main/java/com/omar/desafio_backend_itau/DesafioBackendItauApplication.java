30aeea12da710b88c1259d544ba00fbfbb6aaa04
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-02-03 19:27:23 -0300

    Iniciando projeto com SpringInitializr

---
+++ src/main/java/com/omar/desafio_backend_itau/DesafioBackendItauApplication.java
@@ -0,0 +1,13 @@
+package com.omar.desafio_backend_itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendItauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omar/desafio_backend_itau/DesafioBackendItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.desafio_backend_itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
