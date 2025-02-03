5fdd689aa69104cf2e9f81e2eebf4cf762b2d35c
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-02-03 19:47:08 -0300

    Criando TransacaoService e método para salvar transação

---
+++ src/main/java/com/omar/desafio_backend_itau/services/TransacaoService.java
@@ -0,0 +1,41 @@
+package com.omar.desafio_backend_itau.services;
+
+import com.omar.desafio_backend_itau.dtos.request.TransacaoRequestDTO;
+import com.omar.desafio_backend_itau.exceptions.UnprocessableEntityException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class TransacaoService {
+
+    private final List<TransacaoRequestDTO> transacoes = new ArrayList<>();
+
+    public void salvarTransacao(TransacaoRequestDTO transacaoRequestDTO){
+        log.info("Iniciando processo de salvamento da transação");
+
+        if (transacaoRequestDTO.dataHora() == null || transacaoRequestDTO.valor() == null){
+            log.error("UnprocessableEntityException: dataHora e valor não podem ser nulos");
+            throw new UnprocessableEntityException("atributos dataHora e valor não podem ser nulos");
+        }
+
+        if (transacaoRequestDTO.dataHora().isAfter(OffsetDateTime.now())){
+            log.error("UnprocessableEntityException: data e hora do atributo dataHora devem ser anteriores a data e hora atuais");
+            throw new UnprocessableEntityException("data e hora do atributo dataHora devem ser anteriores a data e hora atuais");
+        }
+
+        if (transacaoRequestDTO.valor() < 0){
+            log.error("UnprocessableEntityException: valor deve ser maior ou igual a 0");
+            throw new UnprocessableEntityException("valor deve ser maior ou igual a 0");
+        }
+
+        transacoes.add(transacaoRequestDTO);
+        log.info("Transação salva com sucesso!");
+    }
+}
