f8cae2402a67be108679f0aafc85cce284925c00
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-02-04 00:39:54 -0300

    Adicionando o record EstatisticaResponseDTO ao EntityUtils

--- src/test/java/com/omar/desafio_backend_itau/utils/EntityUtils.java
+++ src/test/java/com/omar/desafio_backend_itau/utils/EntityUtils.java
@@ -1,6 +1,7 @@
 package com.omar.desafio_backend_itau.utils;
 
 import com.omar.desafio_backend_itau.dtos.request.TransacaoRequestDTO;
+import com.omar.desafio_backend_itau.dtos.response.EstatisticaResponseDTO;
 
 import java.time.OffsetDateTime;
 
@@ -8,5 +9,9 @@ public class EntityUtils {
 
     public static TransacaoRequestDTO criarTransacaoRequestDTO(){
         return new TransacaoRequestDTO(678.99D, OffsetDateTime.now().minusSeconds(30));
+    }
+
+    public static EstatisticaResponseDTO criarEstatisticaResponseDTO(){
+        return new EstatisticaResponseDTO(2L, 1357.98D, 678.99D, 678.99D, 678.99D);
     }
 }
